d5d6d1543a119e22408a8ad17d2344ba86929d41
Midstackdev <midstackdev@gmail.com>
2022-09-06 13:55:39 +0000

    show items

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,48 @@
 import React from "react";
+import { useQuery } from "react-query";
+import { Grid, LinearProgress } from "@mui/material";
+import { Wrapper } from "./App.styles";
+import Item from "./components/item/Item";
+
+//Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch(`https://fakestoreapi.com/products`)).json();
 
 const App = () => {
-  return <div className="App">App</div>;
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    "products",
+    getProducts
+  );
+  console.log(data);
+
+  const getTotalItems = () => null;
+  const handleAddToCart = (clickedItem: CartItemType) => null;
+  const handleRemoveFromCart = () => null;
+
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Something went wrong...</div>;
+
+  return (
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map((item) => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
+  );
 };
 
 export default App;
